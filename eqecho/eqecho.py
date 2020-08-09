b74c270da4699351e04a04d682f21f5fbe09c879
Orsenfelt <orsenfelt@gmail.com>
2020-08-09 12:48:00 +0100

    hmm

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -11,25 +11,14 @@ from redbot.core.utils.chat_formatting import box
 class EQEcho(commands.Cog):
 
     def __init__(self):
-        self.config = Config.get_conf(self, identifier="123321123000")
-        default_guild = {
-            "server": 740650364575023127,
-            "channel": 740650365078339667,
-        }
-        self.config.register_guild(**default_guild)
-
-
+        self.server = "740650364575023127"
+        self.channel = "740650365078339667"
         self.db = pymysql.connect("localhost","rampage","6gxby3An5oYA2cP0S5JR80^X&","rampage" )
         self.cursor = self.db.cursor()
+  
 
 
-    @commands.command()
-    async def setserver(self, ctx, new_value):
-        await self.config.guild(ctx.guild).server.set(new_value)
-        await ctx.send("Value of baz has been changed!")    
-
-
-    async def echo(self):
+    async def echo(self,ctx):
         while True:
             self.cursor.execute("SELECT uid,line FROM echo WHERE echoed='0' ORDER BY epoch ASC")
             data = self.cursor.fetchall()
