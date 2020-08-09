56f28e1f5800783ce1a050c18d5640b76770d987
Orsenfelt <orsenfelt@gmail.com>
2020-08-09 17:32:56 +0100

    int

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -41,7 +41,7 @@ class EQEcho(commands.Cog):
 
     @commands.command(name="test", brief="Just Testing")
     async def test(self, ctx):
-        await self._send_echo(ctx,self.channel)
+        await self._send_echo(ctx,int(self.channel))
 
 
 
