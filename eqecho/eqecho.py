fe35f6cd16f37f006fc37c34d086bac4881bc55d
Orsenfelt <orsenfelt@gmail.com>
2020-08-09 13:08:08 +0100

    gah

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -12,24 +12,51 @@ class EQEcho(commands.Cog):
 
     def __init__(self):
         self.server = "740650364575023127"
-        self.channel = "740650364575023127"
+        self.channel = "740650365078339667"
         self.db = pymysql.connect("localhost","rampage","6gxby3An5oYA2cP0S5JR80^X&","rampage" )
         self.cursor = self.db.cursor()
   
 
 
-    async def send_echo(self):
-        
+    async def send_echo(self, ctx):
+        # Grab 2 lines from database, send to discord and mark as sent
         self.cursor.execute("SELECT uid,line FROM echo WHERE echoed='0' ORDER BY epoch ASC LIMIT 2")
         data = self.cursor.fetchall()
-
-        channel = self.channel
+        channel = ctx.get_user(111584658029826048)
+        
         for line in data:
-            print(line[1])
-            await channel.send(line[1])
+            ## Update this line to echoed
+            sql = "UPDATE echo SET echoed='1' WHERE uid='" + line[0] + "'"
+            try:
+                self.cursor.execute(sql)
+                self.db.commit()
+                await channel.send(line[1])
+            except:
+                self.db.rollback()
 
 
-    async def loop_echo(self):
+    async def loop_echo(self, ctx):
+        ## echo new lines every 3 seconds
         while True:
-            await self.send_echo()
+            await self.send_echo(self, ctx)
             await asyncio.sleep(3)
+
+
+    @commands.command(name="test", brief="Just Testing")
+    async def test(self, ctx):
+        
+        self.cursor.execute("SELECT uid,line FROM echo WHERE echoed='0' ORDER BY epoch ASC LIMIT 5")
+        data = self.cursor.fetchall()
+        author = ctx.author
+
+        for line in data:
+
+            ## Update this line to echoed
+            sql = "UPDATE echo SET echoed='1' WHERE uid='" + line[0] + "'"
+            try:
+                self.cursor.execute(sql)
+                self.db.commit()
+                await ctx.send(line[1])
+            except:
+                self.db.rollback()
+            
