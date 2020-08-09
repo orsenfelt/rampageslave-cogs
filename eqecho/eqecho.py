4409c540a339343d27a8703d0d733de6aad7491b
Orsenfelt <orsenfelt@gmail.com>
2020-08-09 18:08:36 +0100

    g

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -14,17 +14,14 @@ class EQEcho(commands.Cog):
         self.channel = "740650365078339667"
         self.db = pymysql.connect("localhost","rampage","6gxby3An5oYA2cP0S5JR80^X&","rampage" )
         self.cursor = self.db.cursor()
+        
 
-    def _get_lines(self):
+    async def send_echo(self, ctx, channel):
+        
+        channel = ctx.bot.get_channel(int(channel))
+
         self.cursor.execute("SELECT uid,line FROM echo WHERE echoed='0' ORDER BY epoch ASC LIMIT 2")
         data = self.cursor.fetchall()
-        return data
-        
-        
-
-    async def send_echo(self, ctx, channel: discord.TextChannel = "740650365078339667"):
-        
-        
 
         for line in data:
             ## Update this line to echoed
@@ -46,9 +43,6 @@ class EQEcho(commands.Cog):
 
     @commands.command(name="test", brief="Just Testing")
     async def test(self, ctx):
-        data = self._get_lines()
-        for line in data:
-            channel = ctx.bot.get_channel(int(self.channel))
-            await channel.send(line[1])
+        self.send_echo(ctx,self.channel)
 
 
