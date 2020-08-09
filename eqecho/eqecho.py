ab29fc75a20a7472d0815313905c4f027a9f05a6
Orsenfelt <orsenfelt@gmail.com>
2020-08-09 11:35:44 +0100

    dadjoke

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -18,3 +18,15 @@ class EQEcho(commands.Cog):
     async def test(self, ctx):
         author = ctx.author
         await author.send("Hello World")
+
+    @commands.command()
+    async def dadjoke(self, ctx: commands.Context):
+        headers = {
+            "User-Agent": "FoxV3 (https://github.com/bobloy/Fox-V3)",
+            "Accept": "application/json",
+        }
+
+        async with aiohttp.ClientSession(headers=headers) as session:
+            joke = await fetch_url(session, "https://icanhazdadjoke.com/")
+
+        await ctx.maybe_send_embed(joke["joke"]
