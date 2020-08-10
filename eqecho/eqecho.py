d18dc80d52516ebfba3e25053864f735f6979433
Orsenfelt <orsenfelt@gmail.com>
2020-08-10 17:47:30 +0100

    ratelimits

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -30,7 +30,7 @@ class EQEcho(commands.Cog):
         now = str(time.time_ns())
         now = int(now[:-9])
         minago = str(now - (60 * 15))
-        sql = "SELECT id,line FROM echo WHERE epoch>'" + minago + "' ORDER BY id ASC LIMIT 5"
+        sql = "SELECT id,line FROM echo WHERE echoed='0' AND epoch>'" + minago + "' ORDER BY id ASC LIMIT 5"
         print("[~] " + sql)
         self.cursor.execute(sql)
         data = self.cursor.fetchall()
@@ -43,7 +43,7 @@ class EQEcho(commands.Cog):
                 self.db.commit()
                 self.echo_chan.send(line[1])
                 print("[^] Done")
-                sleep(1)
+                sleep(0.6)
             except:
                 self.db.rollback()
 
