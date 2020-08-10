a6412242f94a881350d6aa854752df070e110df6
Orsenfelt <orsenfelt@gmail.com>
2020-08-10 16:47:58 +0100

    intstring

--- eqecho/eqecho.py
+++ eqecho/eqecho.py
@@ -35,9 +35,9 @@ class EQEcho(commands.Cog):
         data = self.cursor.fetchall()
 
         for line in data:
-            print("[~] ("{}") " + line[1]).format(line[0])
+            print("[~] (" + string(line[0]) + ") " + line[1])
             ## Update this line to echoed
-            sql = "UPDATE echo SET echoed='1' WHERE id='{}'".format(line[0])
+            sql = "UPDATE echo SET echoed='1' WHERE id='" + string(line[0]) + "'"
             try:
                 self.cursor.execute(sql)
                 self.db.commit()
